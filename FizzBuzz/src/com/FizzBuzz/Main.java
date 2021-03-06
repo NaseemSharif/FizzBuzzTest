dbde85f3f118504d77f9017c575f46b4cdd0d25e
NaseemSharif <67925146+NaseemSharif@users.noreply.github.com>
2021-03-06 15:54:27 +0000

    added FizzBuzz test

---
+++ FizzBuzz/src/com/FizzBuzz/Main.java
@@ -0,0 +1,25 @@
+package com.FizzBuzz;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("FizzBuzz");
+        System.out.print("Please Enter a Number: ");
+        int fizzBuzz = scanner.nextInt();
+
+        if (fizzBuzz % 5 == 0 && fizzBuzz % 3 == 0){
+            System.out.println("FizzBuzz");
+        }
+        else if(fizzBuzz % 5 == 0){
+            System.out.println("Fizz");
+        }
+        else if (fizzBuzz % 3 == 0){
+            System.out.println("Buzz");
+        }
+        else System.out.println(fizzBuzz + " divided by 5 or 3 is not a whole number");
+    }
+}
